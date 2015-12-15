3f76b6ed564d20090373ba896dab74d0cbcac83c
Jess <jessreiszel@gmail.com>
2015-12-15 01:41:52 -0500

    Added the main screen XML and the carousel. Just needs functionality.

--- app/src/main/java/sjcnymobile/supercoolbusinesscardapp/MainActivity.java
+++ app/src/main/java/sjcnymobile/supercoolbusinesscardapp/MainActivity.java
@@ -1,13 +1,76 @@
 package sjcnymobile.supercoolbusinesscardapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import sjcnymobile.supercoolbusinesscardapp.CarouselAdapter.OnItemClickListener;
+import sjcnymobile.supercoolbusinesscardapp.CarouselAdapter.OnItemSelectedListener;
 
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Carousel carousel = (Carousel)findViewById(R.id.carousel);
+        TypedArray images = getResources().obtainTypedArray(R.array.entries);
+
+        CarouselImageAdapter imageAdapter=new CarouselImageAdapter(this, images);
+        carousel.setCarouselAdapter(imageAdapter);
+
+        carousel.setOnItemClickListener(new OnItemClickListener(){
+
+            public void onItemClick(CarouselAdapter<?> parent, View view,
+                                    int position, long id) {
+
+                Toast.makeText(MainActivity.this,
+                        String.format("%s has been clicked",
+                                ((CarouselItem)parent.getChildAt(position)).getName()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        carousel.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+                                               public void onItemSelected(CarouselAdapter<?> parent, View view,
+                                                                          int position, long id) {
+
+                                                  // final TextView txt = (TextView)(findViewById(R.id.selected_item));
+
+                                                   switch(position){
+                                                       case 0:
+
+                                                           break;
+                                                       case 1:
+
+                                                           break;
+                                                       case 2:
+
+                                                           break;
+                                                       case 3:
+
+                                                           break;
+                                                       case 4:
+
+                                                           break;
+                                                       case 5:
+
+                                                           break;
+                                                   }
+
+                                               }
+
+                                               public void onNothingSelected(CarouselAdapter<?> parent) {
+                                               }
+
+                                           }
+        );
+
     }
+
 }
+
